846be218c7a25c5eb4150f98920c12c28cc82008
Udipto <131182197+Udipto-Mondal@users.noreply.github.com>
2023-12-21 09:25:25 +0600

    Add files via upload

---
+++ Greater Average.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+int t;
+	cin>>t;
+	while(t--){
+
+	    double a,b,c;
+	    cin>>a>>b>>c;
+	    double av=(a+b)/2;
+	    if (av>c)
+	    cout<<"YES"<<endl;
+
+	    else
+	    cout<<"NO"<<endl;
+	}
+
+return 0;
+}
