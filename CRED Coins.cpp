b1a065613d02b62a353ab5ec1efd1606eea7434f
Udipto <131182197+Udipto-Mondal@users.noreply.github.com>
2024-03-08 00:36:32 +0600

    Add files via upload

---
+++ CRED Coins.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+
+	    int x,y;
+	    cin>>x>>y;
+	   int s= (x*y)/100;
+	   cout<<s<<endl;
+	}
+
+}
+
