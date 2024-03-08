0a213cedf8a8fdb62bf1b6065f5ccb1cf5de106a
Udipto <131182197+Udipto-Mondal@users.noreply.github.com>
2024-03-08 22:25:51 +0600

    Add files via upload

---
+++ Chefland Games.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int  t;
+cin>>t;
+while(t--){
+    int r1,r2,r3,r4;
+    cin>>r1>>r2>>r3>>r4;
+    if((r1+r2+r3+r4)==0)
+    cout<<"IN"<<"\n";
+  else
+    cout<<"OUT"<<"\n";
+}
+return 0;
+}
