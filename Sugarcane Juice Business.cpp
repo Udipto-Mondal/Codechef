cf9887ad3afd06d39f17d6781e68472b2103a5a6
Udipto <131182197+Udipto-Mondal@users.noreply.github.com>
2024-03-13 00:53:58 +0600

    Add files via upload

---
+++ Sugarcane Juice Business.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--){
+
+    int n;
+    cin>>n;
+    int s=n*50;
+    int p=s-(n*10+n*10+n*15);
+    cout<<p<<"\n";
+
+}
+return 0;
+}
